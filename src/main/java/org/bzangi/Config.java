d2433237da3dfd1633aa557d02d7e197258da41c
Bruno <bruno_zangi@hotmail.com>
2022-09-13 21:47:48 -0300

    First commit

---
+++ src/main/java/org/bzangi/Config.java
@@ -0,0 +1,13 @@
+package org.bzangi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+    @Bean(name = "applicationName")
+    public String applicationName(){
+        return "Sales system";
+    }
+}
